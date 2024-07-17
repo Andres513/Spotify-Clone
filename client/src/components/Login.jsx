89dba863ef21b1b2696afa61d977410c1e15fb0b
Andres513 <andres.mares@outlook.com>
2024-07-17 12:59:10 -0700

    Updated code to ask the user to assign their client ID to the client_ID variable

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=41d057fa0a1841388bf0cafb1110a811&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+const client_ID = 'Enter your client ID here'
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
-export default function Login() {
+export default function Login(){
   return (
     <>
-      <div className='button-container'>
-        <a className='login-button' href={AUTH_URL}>Log Into Spotify</a>
+    <div className='button-container'>
+      <a className='login-button' href={AUTH_URL}>Log Into Spotify</a>
     </div>
     </>
   )
 }
+ 
