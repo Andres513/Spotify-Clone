2bb94d89e8be5ed8b0b8ce60dcf29f877367d1ff
Andres513 <andres.mares@outlook.com>
2024-10-08 13:52:18 -0700

    Commit: Reorganized server.js to be more readable

--- server/server.js
+++ server/server.js
@@ -6,49 +6,49 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.json())
 app.use(bodyParser.json())
 
-app.post('/refresh', (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken
-})
-
-spotifyApi.refreshAccessToken().then(data => {
-  accessToken = data.body.access_token
-  expiresIn = data.body.expiresIn
-
-}).catch((error) => {
-  console.error(error)
-  res.sendStatus(400)
-  })
-})
-
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const code = req.body.code
-  if (!code) {
-    return res.status(400).send('Code must be supplied');
-  }
-
-
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
   })
+
   spotifyApi.authorizationCodeGrant(code).then(data => {
     res.json({
       accessToken: data.body.access_token,
       refreshToken: data.body.refresh_token,
       expiresIn: data.body.expires_in,
     })
-  }).catch((error)=> {
-    console.error(error)
-    res.sendStatus(400)
   })
+    .catch(error => {
+      console.log(error)
+      res.sendStatus(400)
+    })
 })
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken
+  })
+
+  spotifyApi.refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.error(err)
+      res.sendStatus(400)
+    })
+})
+
 app.listen(3001)
