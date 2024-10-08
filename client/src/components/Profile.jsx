9a58c68ae75ab5e442f2fb4d147fd6ae3f5eb6b6
Andres513 <andres.mares@outlook.com>
2024-10-08 13:57:04 -0700

    Commit: Added try/catch data pull for profile info and renders

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
-import SpotifyWebApi from 'spotify-web-api-node'
+import { useState, useEffect } from 'react'
 
-export default function() {
+export default function Profile({ accessToken }) {
+  const [displayName, setDisplayName] = useState('')
+  const [email, setEmail] = useState('')
+  useEffect(() => {
+
+    const getProfile = async () => {
+      if (!accessToken) return
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        })
+        const req = await response.json()
+        setDisplayName(req.display_name)
+        setEmail(req.email)
+      } catch (err) {
+        console.error('Error getting user profile: ', err)
+      }
+    }
+    getProfile()
+  }, [accessToken])
+
+  const handleLogOut = (event) => {
+    event.preventDefault()
+    window.location = '/'
+  }
 
   return (
     <>
-    
+      <div className='profile-container'>
+        <div className="p-item">Welcome, {displayName}!</div>
+        <div className="p-item">{email}</div>
+        <div className="p-item">Search for a song!</div>
+        <button onClick={handleLogOut} className="logout-button">Log Out</button>
+      </div>
     </>
   )
 }
