af54b04ee492ec425982a392f0d66549fec667d6
Andres513 <andres.mares@outlook.com>
2024-10-08 13:58:23 -0700

    Commit: Organized code to be more readable

--- client/src/components/TrackResults.jsx
+++ client/src/components/TrackResults.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 
-export default function TrackResults({ track, chooseTrack, setClicked }) {
-   
+export default function TrackResults({ track, chooseTrack, setTrackSelected }) {
+
     function handlePlay() {
         chooseTrack(track)
-        setClicked(true)
+        setTrackSelected(true)
     }
-    
-  return (
-    <>
 
-
- <div className='search-result-list'
-    onClick={handlePlay}>
-        <img src ={track.albumUrl} style={{height: '64px', width: '64px'}}/>
-        <div className='result-item'>
-            <div className='track-title'>{track.title}</div>
-            <div className='artist-title'>{track.artist}</div>
-        </div>
-    </div>
-    </> 
-  )
-  }
+    return (
+        <>
+            <div className='search-result-list'
+                onClick={handlePlay}>
+                <img src={track.albumUrl} style={{ height: '64px', width: '64px' }} />
+                <div className='result-item'>
+                    <div className='track-title'>{track.title}</div>
+                    <div className='artist-title'>{track.artist}</div>
+                </div>
+            </div>
+        </>
+    )
+}
