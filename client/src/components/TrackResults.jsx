86aac1ab86f76fbea7a3c1e74648cd65e2c78995
Andres513 <andres.mares@outlook.com>
2024-10-15 13:56:43 -0700

    remnoved fragements

--- client/src/components/TrackResults.jsx
+++ client/src/components/TrackResults.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 
-export default function TrackResults({ track, chooseTrack, setTrackSelected }) {
+export default function TrackResults({ track, chooseTrack }) {
 
-    function handlePlay() {
+    const handlePlay = () => {
         chooseTrack(track)
-        setTrackSelected(true)
     }
 
     return (
-        <>
-            <div className='search-result-list'
-                onClick={handlePlay}>
-                <img src={track.albumUrl} style={{ height: '64px', width: '64px' }} />
-                <div className='result-item'>
-                    <div className='track-title'>{track.title}</div>
-                    <div className='artist-title'>{track.artist}</div>
-                </div>
+        <div className='search-result-list'
+            onClick={handlePlay}>
+            <img src={track.albumUrl} style={{ height: '64px', width: '64px' }} />
+            <div className='result-item'>
+                <div className='track-title'>{track.title}</div>
+                <div className='artist-title'>{track.artist}</div>
             </div>
-        </>
+        </div>
     )
 }
