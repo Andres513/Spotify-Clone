682f6c0111edca50b37b277487aecd186f482485
Andres513 <andres.mares@outlook.com>
2024-10-15 13:59:52 -0700

    Replaced ? operator with conditional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
 
-  return (
-    <>
-      {
-        code ? ( <Dashboard code ={code}/> ) : ( <Login/> )
-      }
-    </>
-  )
+  if (!code) {
+    return <Login />
+  }
+  return <Dashboard code={code} />
 }
 
 export default App
