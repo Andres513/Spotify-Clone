d698a94da0f5a07770ff9baf5b57a6dc46ce22cf
Andres513 <andres.mares@outlook.com>
2024-10-15 13:37:25 -0700

    Updated dashboard: Replaced form/submit with onKeyDown

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import UseAuth from './UseAuth'
+import useAuth from './UseAuth'
 import TrackResults from './TrackResults'
 import Player from './Player'
 import Profile from './Profile'
@@ -7,18 +7,19 @@ import { useState, useEffect } from 'react'
 import SpotifyWebApi from 'spotify-web-api-node'
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: 'Add your client ID here'
+    clientId: '41d057fa0a1841388bf0cafb1110a811'
 })
 
 export default function Dashboard({ code }) {
-    const accessToken = UseAuth({ code })
+    const accessToken = useAuth({ code })
     const [search, setSearch] = useState("")
     const [searchResults, setSearchResults] = useState([])
     const [currentTrack, setCurrentTrack] = useState()
     const [backImage, setBackImage] = useState()
-    const [trackSelected, setTrackSelected] = useState(false)
+    let trackSelected = !!currentTrack
 
-    function chooseTrack(track) {
+
+    const chooseTrack = (track) => {
         setCurrentTrack(track)
         setBackImage(track.albumUrl)
         setSearch('')
@@ -29,65 +30,62 @@ export default function Dashboard({ code }) {
         spotifyApi.setAccessToken(accessToken)
     }, [accessToken])
 
+    const onKeyDownHandler = (e) => {
+        if (!search) return
+        if (e.keyCode === 13) {
+            spotifyApi.searchTracks(search).then(res => {
+                const results = res.body.tracks.items.map(track => {
+                    const largestAlbum = track.album.images.reduce((largest, image) => {
+                        if (image.height > largest) return image
+                        return largest
+                    }, track.album.images[0])
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        if (trackSelected) setTrackSelected(false)
-        if (!search) return setSearchResults([])
-        if (!accessToken) return
-        let cancel = false
-
-        spotifyApi.searchTracks(search).then(res => {
-            if (cancel) return
-            setSearchResults(res.body.tracks.items.map(track => {
-                const largestAlbumImage = track.album.images.reduce((largest, image) => {
-                    if (image.height > largest.height) return image
-                    return largest
-                }, track.album.images[0])
-
-                return {
-                    artist: track.artists[0].name,
-                    title: track.name,
-                    album: track.album.name,
-                    uri: track.uri,
-                    albumUrl: largestAlbumImage.url
-                }
-            }))
-        })
-    }
-    return (
-        <>
-            <div className='dashboard-container'>
-                <div className="profile-container">
-                    <Profile code={code} accessToken={accessToken} />
-                </div>
-                <form className="search-bar" onSubmit={handleSubmit}>
-                    <input type='search' placeholder='Search for Song/Artist' value={search}
-                        onChange={e => setSearch(e.target.value)} />
-                </form>
-                <div className="results-container">
-                    {
-                        !trackSelected ? (
-                            searchResults.map(track => (
-                                <TrackResults track={track} key={track.uri} setTrackSelected={setTrackSelected}
-                                    chooseTrack={chooseTrack} />))
-                        ) : (
-                            <>
-                                <div className="album-cover">
-                                    <img src={backImage} alt="Album Cover" />
-                                    <h2>{currentTrack?.artist} - {currentTrack?.title}</h2>
-                                    <h2>{currentTrack?.album}</h2>
-                                    <button className="return-button" onClick={() => setTrackSelected(false)}>Back</button>
-                                </div>
-                            </>
-                        )
+                    return {
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        album: track.album.name,
+                        uri: track.uri,
+                        albumUrl: largestAlbum.url
                     }
-                </div>
-                <div className='player'>
-                    <Player accessToken={accessToken} trackUri={currentTrack?.uri} />
-                </div>
+                })
+                setSearchResults(results)
+            })
+        }
+    }
+
+    const onChangeHandler = (e) => {
+        setSearch(e.target.value)
+    }
+    const returnHandler = (e) => {
+        setCurrentTrack(null)
+    }
+
+    return (
+        <div className='dashboard-container'>
+            <div className="profile-container">
+                <Profile code={code} accessToken={accessToken} />
             </div>
-        </>
+            <input className="search-bar" type='search' placeholder='Search for Song/Artist' value={search}
+                onChange={onChangeHandler} onKeyDown={onKeyDownHandler} />
+            <div className="results-container">
+                {
+                    !trackSelected ? (
+                        searchResults.map(track => (
+                            <TrackResults track={track} key={track.uri}
+                                chooseTrack={chooseTrack} trackSelected={trackSelected} />))
+                    ) : (
+                        <div className="album-cover">
+                            <img src={backImage} alt="Album Cover" />
+                            <h2>{currentTrack?.artist} - {currentTrack?.title}</h2>
+                            <h3>{currentTrack?.album}</h3>
+                            <button className="return-button" onClick={returnHandler}>Back</button>
+                        </div>
+                    )
+                }
+            </div>
+            <div className='player'>
+                <Player accessToken={accessToken} trackUri={currentTrack?.uri} />
+            </div>
+        </div>
     )
 }
