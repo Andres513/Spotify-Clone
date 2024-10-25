ca4d607b176a8bc5fe3c253aa5198ce5685c463a
Andres513 <andres.mares@outlook.com>
2024-10-24 22:41:30 -0700

    moved instruction comments to App.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,22 +1,3 @@
-// You must create an app from developer.spotify.com and set the redirect URI to http://localhost:3000
-
-// 1. Go into client >> .env >> enter you client ID from your spotify app basic information settings
-
-// 2. Go into server >> .env >> enter your CLIENT_ID and CLIENT_SECRET values from your spotify app basic information settings
-
-// 3. In terminal, cd into client folder and type the following command
-//     npm i
-
-// 4. Then cd into server folder and run the following:
-//     npm i
-
-// 5. To run app, open terminal and cd into client folder and run the following:
-//     npm run dev
-
-// 6. Then open up another terminal window and cd into server folder and run the following:
-//     npm run devStart
-
-
 import React from 'react'
 import useAuth from './UseAuth'
 import TrackResults from './TrackResults'
