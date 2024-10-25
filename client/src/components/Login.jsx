c9528188e5bdf326ce2f86379b929d602a63ae82
Andres513 <andres.mares@outlook.com>
2024-10-24 22:39:50 -0700

    imported client ID enviroment variable from .env file

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const client_ID = '41d057fa0a1841388bf0cafb1110a811'
+const client_ID = import.meta.env.VITE_CLIENT_ID
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
